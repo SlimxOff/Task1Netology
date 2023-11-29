2de4cdb074313ce6f5228e172d1fb82a8773dfd2
ivangoyda <iv.goyda.work@gmail.com>
2023-11-29 15:06:40 +0300

    First commit

---
+++ src/Calculator.java
@@ -0,0 +1,27 @@
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+
+    // Добавлен блок проверки деления на ноль
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y != 0) {
+            return x / y;
+        } else {
+            throw new ArithmeticException("На ноль делить нельзя");
+        }
+    };
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+}
